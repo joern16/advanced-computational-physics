19c514942adbdb789abe7c1c777bdac94007d8e9
Joern Wilhelm <joern.wilhelm@web.de>
2026-04-09 20:56:10 +0200

    simplified code. added numba for faster random walk

--- assignment-4/assignment-4.py
+++ assignment-4/assignment-4.py
@@ -14,35 +14,13 @@ from parallel_statistics import ParallelMeanVariance
 
 from numba import njit
 
-
-def ising_lattice_init(L, random=True):
-    """
-    Initializes an L x L lattice.
-    """
-    if random:
-        return np.random.choice([-1, 1], size=(L, L))
-    return np.ones((L, L), dtype=int)
-
-def ising_energy(lattice, J=1.0, H=0.0):
-    """
-    Calculate total energy of ising lattice with periodic BC.
-    """
-    L = lattice.shape[0]
-    energy = 0
-    for i in range(L):
-        for j in range(L):
-            spin = lattice[i, j]
-            # Only sum right and down to avoid double-counting pairs
-            neighbor_sum = (lattice[(i + 1) % L, j] + lattice[i, (j + 1) % L])
-            energy += -J * spin * neighbor_sum - H * spin
-    return energy
-
-
-def ising_walker(L, T, J=1.0, H=0.0, burn_in=1000, steps=10000, plot=False):
+@njit
+def ising_walker(L, T, J=1.0, H=0.0, burn_in=1000, steps=10000):
     """
     Perform Metropolis sampling at temperature T.
     """
-    lattice = ising_lattice_init(L)
+    # Initialize lattice
+    lattice = np.random.choice([-1, 1], size=(L, L))
     
     # Burn-in period
     for _ in range(burn_in):
@@ -53,13 +31,19 @@ def ising_walker(L, T, J=1.0, H=0.0, burn_in=1000, steps=10000, plot=False):
         if energy_difference <= 0 or np.random.rand() < np.exp(-energy_difference / T):
             lattice[i, j] *= -1
 
-    # Sampling period
+    # Calculate energy and magnetization
     energies = np.zeros(steps + 1)
-    magnetizations = np.zeros(steps + 1)
+    for i in range(L):
+        for j in range(L):
+            spin = lattice[i, j]
+            # Only sum right and down to avoid double-counting pairs
+            neighbor_sum = (lattice[(i + 1) % L, j] + lattice[i, (j + 1) % L])
+            energies[0] += -J * spin * neighbor_sum - H * spin
 
-    energies[0] = ising_energy(lattice, J, H)
+    magnetizations = np.zeros(steps + 1)
     magnetizations[0] = np.sum(lattice)
 
+    # Sampling period
     for iteration in range(steps):
         i, j = np.random.randint(L, size=2)
         energy_difference = 2 * lattice[i, j] * (J * (lattice[(i + 1) % L, j] + lattice[(i - 1) % L, j] + 
@@ -72,9 +56,26 @@ def ising_walker(L, T, J=1.0, H=0.0, burn_in=1000, steps=10000, plot=False):
         else:
             energies[iteration + 1] = energies[iteration]
             magnetizations[iteration + 1] = magnetizations[iteration]
+    
+    return energies, magnetizations
 
-    # Plot energies and magnetizations
-    if plot:
+def ising_wrapper(L, T, J=1.0, H=0.0, burn_in=1000, steps_per_core=10000, plot_walk=False):
+    """
+    Run Metropolis sampling in parallel and calculate mean energy, mean magnetization, heat capacity, and magnetic susceptibility.
+    """
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+
+    energies, magnetizations = ising_walker(L, T, J, H, burn_in, steps_per_core)
+
+    calc = ParallelMeanVariance(size=2)
+    calc.add_data(0, energies)
+    calc.add_data(1, magnetizations)
+
+    _, mean, var = calc.collect(comm=comm, mode="gather")
+
+    # Plot energies and magnetizations for single walker
+    if plot_walk and rank == 0:
         plt.figure(figsize=(8, 6))
         plt.plot(energies)
         plt.title("energies")
@@ -84,58 +85,14 @@ def ising_walker(L, T, J=1.0, H=0.0, burn_in=1000, steps=10000, plot=False):
         plt.plot(magnetizations)
         plt.title("magnetizations")
         plt.savefig("magnetizations")
-    
-    return energies, magnetizations
 
-def ising_walker_parallel(L, T, J=1.0, H=0.0, burn_in=1000, steps_per_core=10000, plot_walk=False):
-    """
-    Perform parallel Metropolis sampling at temperature T.
-    """
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-
-    energies, magnetizations = ising_walker(L, T, J, H, burn_in, steps_per_core, plot_walk)
-
-    calc = ParallelMeanVariance(size=2)
-    calc.add_data(0, energies)
-    calc.add_data(1, magnetizations)
-
-    _, mean, variance = calc.collect(comm=comm, mode="gather")
-
+    # Return mean and variance (in units of kB)
     if rank == 0:
-        mean_energy = mean[0]
-        mean_magnetization = mean[1]
-        var_energy = variance[0]
-        var_magnetization = variance[1]
-
-        # The heat capacity and magnetic susceptibility is in units of kB
-        return mean_energy, mean_magnetization, var_energy / (T**2), var_magnetization / T    
-
-    else:
-        return None, None, None, None
-
-def ising_wrapper(L, T, J=1.0, H=0.0, burn_in=1000, steps_per_core=10000, plot_walk=False):
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-
-    mean_energy, mean_magnetization, heat_capacity, magnetic_susceptibility = ising_walker_parallel(L, T, J, H, burn_in, steps_per_core, plot_walk)
-
-    if rank == 0:
-        print("Mean energy: ", mean_energy)
-        print("Mean magnetization: ", mean_magnetization)
-        print("Heat capacity: ", heat_capacity)
-        print("Magnetic susceptibility: ", magnetic_susceptibility)
-
-
+        print("Mean energy: ", mean[0])
+        print("Mean magnetization: ", mean[1])
+        print("Heat capacity: ", var[0] / (T**2))
+        print("Magnetic susceptibility: ", var[1] / T)
 
     
 if __name__ == "__main__":
-    L = 16
-    T = 1.0
-
-    ising_wrapper(L, T, J=1.0, H=5.0, burn_in=10000, steps_per_core=100000)
-
-    
-
-
-
+    ising_wrapper(L=16, T=1.0, J=1.0, H=0.0, burn_in=10000, steps_per_core=100000, plot_walk=True)
