84263ea458cedc11e3faffcd70f4f3534a1cfe1a
Jorn Wilhelm <gbb25224@archie-t.hpc.strath.ac.uk>
2026-04-09 10:49:46 +0100

    added jobscript

---
+++ assignment-4/assignment-4.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#======================================================
+#
+# Job script for running LAMMPS on multiple nodes
+#
+#======================================================
+
+#======================================================
+# Propogate environment variables to the compute node
+#SBATCH --export=ALL
+#
+# Run in the teaching partition (queue)
+#SBATCH --partition=teaching
+#
+# Specify project account
+#SBATCH --account=teaching
+#
+# No. of tasks required
+#SBATCH --ntasks=1
+#
+# Distribute processes in round-robin fashion for load balancing
+#SBATCH --distribution=block:block
+#
+# Specify (hard) runtime (HH:MM:SS)
+#SBATCH --time=01:00:00
+#
+# Job name
+#SBATCH --job-name=assignment-1
+#
+# Output file
+#SBATCH --output=slurm-%j.out
+#======================================================
+
+module purge
+
+module load mpi
+
+perf stat -e cycles,instructions,cache-misses mpirun -np 1 python3 assignment-4.py 
